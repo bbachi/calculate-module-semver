52209417e569ba95434a19ee377186383a000b8a
Bhargav Bachina <bhargavbachina@yahoo.com>
2019-11-29 17:19:35 -0600

    added function name for additional features

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-var Calculate = function(a, b) {
+var Calculate = function(a, b, functionName) {
+    if (functionName === 'add') {
+        return a + b;
+    } 
+    if (functionName === 'subtract') {
+        return a - b;
+    } 
+    if (functionName === 'multiply') {
+        return a * b;
+    } 
     return a + b;
 }
 
